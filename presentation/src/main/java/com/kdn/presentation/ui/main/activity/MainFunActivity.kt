d6c46c3debf81b826f8472ab2ebf667e5768bfef
choiyunsung <12345ggh81@gmail.com>
2021-11-14 02:33:22 +0900

    [CREATE] UI 업데이트

---
+++ presentation/src/main/java/com/kdn/presentation/ui/main/activity/MainFunActivity.kt
@@ -0,0 +1,12 @@
+package com.kdn.presentation.ui.main.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kdn.presentation.R
+
+class MainFunActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_fun)
+    }
+}
---
+++ presentation/src/main/java/com/kdn/presentation/ui/setting/fragment/ChooseTreeFragment.kt
@@ -0,0 +1,21 @@
+package com.kdn.presentation.ui.setting.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kdn.presentation.R
+
+
+class ChooseTreeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_choose_tree, container, false)
+    }
+
+}
