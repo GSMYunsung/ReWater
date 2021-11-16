0dacb640aa43fd60dee6747836c36348e7a52618
choiyunsung <12345ggh81@gmail.com>
2021-11-17 00:27:00 +0900

    [CREATE] 새로운 UI 생성

---
+++ presentation/src/main/java/com/kdn/presentation/ui/main/fragment/SetTimeAndOutputFragment.kt
@@ -0,0 +1,20 @@
+package com.kdn.presentation.ui.main.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kdn.presentation.R
+
+class SetTimeAndOutputFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_set_time_and_output, container, false)
+    }
+
+}
