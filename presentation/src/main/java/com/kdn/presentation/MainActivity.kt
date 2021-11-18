052b04427210ce1d244ce5a6f3a7ad3c497ed4a6
choiyunsung <12345ggh81@gmail.com>
2021-11-18 20:04:42 +0900

    [UPDATE] ui 로직 업데이트

--- presentation/src/main/java/com/kdn/presentation/MainActivity.kt
+++ presentation/src/main/java/com/kdn/presentation/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.kdn.presentation
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingSuperCall")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- presentation/src/main/java/com/kdn/presentation/ui/main/activity/MainFunActivity.kt
+++ presentation/src/main/java/com/kdn/presentation/ui/main/activity/MainFunActivity.kt
@@ -1,9 +1,14 @@
 package com.kdn.presentation.ui.main.activity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kdn.presentation.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
+
+@SuppressLint("MissingSuperCall")
 class MainFunActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
