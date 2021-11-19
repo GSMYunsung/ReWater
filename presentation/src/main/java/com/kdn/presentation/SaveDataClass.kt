845bd5644043cc102b10e2ddf6525fd9f7ed0ad0
choiyunsung <12345ggh81@gmail.com>
2021-11-20 02:28:39 +0900

    [CRATE] 파이어베이스 리얼타임 데이터베이스의 값을 저장하는 dataclass 추가

---
+++ presentation/src/main/java/com/kdn/presentation/SaveDataClass.kt
@@ -0,0 +1,18 @@
+package com.kdn.presentation
+
+data class SaveDataClass(
+    var Ieast_temperature : String = "",
+    var alarm_o_clock : String = "",
+    var alram_minutes : String = "",
+    var battery : String = "",
+    var current_humidity : String = "",
+    var desired_humidity : String = "",
+    var if_output : Boolean = false,
+    var maximum_temperature : String = "",
+    var output : String = "",
+    var output_time : String = "",
+    var size : String = "",
+    var tree : String = "",
+    var volume : String = ""
+
+)
