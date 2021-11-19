e439c445a44039a44c556950e7798df9dc407316
choiyunsung <12345ggh81@gmail.com>
2021-11-20 02:29:14 +0900

    [UPDATE] 필요없는 코드삭제

--- presentation/src/main/java/com/kdn/presentation/ui/setting/viewmodel/MainViewmodel.kt
+++ presentation/src/main/java/com/kdn/presentation/ui/setting/viewmodel/MainViewmodel.kt
@@ -1,7 +1,10 @@
 package com.kdn.presentation.ui.setting.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.kdn.module.usecase.GetPlantStateUseCase
+import com.kdn.presentation.SaveDataClass
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
