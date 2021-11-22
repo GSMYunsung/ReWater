0064c33e60b40e9ac13bc2a1c8034c57f7525183
choiyunsung <12345ggh81@gmail.com>
2021-11-22 20:47:04 +0900

    [UPDATE] 권한 및 스플래시 화면 추가

--- presentation/src/main/java/com/kdn/presentation/ui/setting/activity/SettingActivity.kt
+++ presentation/src/main/java/com/kdn/presentation/ui/setting/activity/SettingActivity.kt
@@ -10,6 +10,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Design_Light_NoActionBar)
         setContentView(R.layout.activity_setting)
     }
 }
